5dd1e2731fe765007ce9c51c5320bda3e30e2313
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-06-14 09:36:09 +0900

    [fix] model struct

--- models/models.go
+++ models/models.go
@@ -1,16 +1,13 @@
 package models
 
 type Feature struct {
-	ID         string   `json:"id"`
-	Properties Property `json:"properties"`
-	Geometry   Geometry `json:"geometry"`
+	ID         string            `json:"id"`
+	Type       string            `json:"type"`
+	Properties map[string]string `json:"properties"`
+	Geometry   Geometry          `json:"geometry"`
 }
 
 type Geometry struct {
 	Coordinates [][][]float64 `json:"coordinates"`
-	Type        string  `json:"type"`
-}
-
-type Property struct {
-	Title string `json:"title,omitempty"`
+	Type        string        `json:"type"`
 }
